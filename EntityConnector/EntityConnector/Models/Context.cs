ab6745998d7065201c94dd763f9e33596c69e7ac
Alex Kolodziejczak <akolodziejczak11@gmail.com>
2023-02-20 20:51:38 -0500

    remove unused constructor

--- EntityConnector/EntityConnector/Models/Context.cs
+++ EntityConnector/EntityConnector/Models/Context.cs
@@ -3,7 +3,6 @@ namespace EntityConnector.Models
 {
     public class Context : DbContext
     {
-        public Context() : base() { }
         public Context(DbContextOptions<Context> options) : base(options) { }
         public DbSet<NumberData> Data { get; set; }
     }
